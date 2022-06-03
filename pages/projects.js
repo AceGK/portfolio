0be98ee464098c844069aaecd4fc99e31ebabf59
Ace <acekisch@gmail.com>
2022-06-02 18:39:04 -0700

    Update projects.js

--- pages/projects.js
+++ pages/projects.js
@@ -17,7 +17,7 @@ export default class ProjectData extends Component {
         <i className="icon-css31" title="CSS3" key="2"></i>,
         <i className="icon-react" title="React" key="3"></i>,
         <i className="icon-node-dot-js" title="Node js" key="4"></i>,
-        <i className="icon-netlify" title="Netlify" key="5"></i>,
+        <i className="icon-vercel" title="Vercel" key="5"></i>,
       ], 
       description: 'My portfolio site, showcasing my work and personal projects', 
       buttons: [ 
@@ -36,7 +36,7 @@ export default class ProjectData extends Component {
         <i className="icon-javascript" title="Javascript ES6" key="3"></i>,
         <i className="icon-wordpress1" title="Wordpress" key="4"></i>
       ],   
-      description:'Website for a cannabis dispensary located in San Jose, CA.', 
+      description:'Website for a cannabis dispensary located in San Jose, CA. Currently being rebuilt with Next.js.', 
       buttons: [ 
         <a href="https://plpcsanjose.com" className="btn" target="_blank" rel="noreferrer" key="1">
             View Site
@@ -51,14 +51,11 @@ export default class ProjectData extends Component {
         <i className="icon-rainmeter2" title="Rainmeter" key="1"></i>,
         <i className="icon-lua" title="LUA" key="2"></i>
       ],   
-      description: 'Rainmeter skin modeled after HAL 9000 that monitors PC performance.', 
+      description: 'Rainmeter skin modeled after HAL 9000 that monitors PC performance status and details.', 
       buttons: [ 
         <a href="https://github.com/AceGK/Hal9000-Rainmeter" className="btn" target="_blank" rel="noreferrer" key="1">
             <i className="icon-github" title="Github"></i> Github
          </a>,
-         <a href="https://github.com/AceGK/Hal9000-Rainmeter" className="btn" target="_blank" rel="noreferrer" key="2">
-         <i className="icon-github" title="Github"></i> Github
-      </a>,
       ],
       id: 3 
     }
