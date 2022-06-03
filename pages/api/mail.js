096b2258032ffe86e805dc9b622b0a0b3134b70d
Ace <acekisch@gmail.com>
2022-06-02 18:24:08 -0700

    added sendgrid api, still needs domain setup

---
+++ pages/api/mail.js
@@ -0,0 +1,25 @@
+const mail = require('@sendgrid/mail'); 
+
+mail.setApiKey(process.env.SENDGRID_API_KEY);
+
+export default (req, res) => {
+    const body = JSON.parse(req.body);
+    console.log(body);
+
+    const message = `
+    Name: ${body.name}\r\n
+    Email: ${body.email}\r\n
+    Message: ${body.message}
+    `;
+
+    const data = {
+        to: 'acekisch@gmail.com',
+        from: 'acekisch@gmail.com',
+        subject: 'New portfolio contact message',
+        text: message, 
+        html: message.replace(/\r\n/g, '<br>')
+    };
+
+    mail.send(data)
+    res.status(200).json({ status: 'Ok' })
+  }
--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,33 @@
 import React from "react";
 
 function Contact() {
+  async function handleOnSubmit(e) {
+    e.preventDefault();
+    const formData = {}
+    Array.from(e.currentTarget.elements).forEach(field => {
+      if ( !field.name ) return; 
+      formData[field.name] = field.value;
+    });
+    fetch('/api/mail', {
+      method: 'post',
+      body: JSON.stringify(formData)
+    })
+    console.log(formData);
+  }
+
   return (
-    
+  
+ 
     <div id="contact" className="page-container">
-        <form name="contact" method="POST" data-netlify="true" action="/contact">
+
+    <style jsx>{`
+        label{ 
+          display:block;
+        }
+        
+        `}
+    </style>
+        <form name="contact" method="POST" onSubmit={handleOnSubmit}>
             <input type="hidden" name="form-name" value="contact" />
             <label htmlFor="name" hidden>Name</label>
             <input type="text" name="name" placeholder="Name" required/>
@@ -14,10 +37,7 @@ function Contact() {
 
             <label htmlFor="message" hidden>Message</label>
             <textarea name="message" placeholder="Message" required></textarea>
-
-            {/* <div data-netlify-recaptcha="true"></div> */}
-            <br></br>
-            <button type="submit" className="project-btn">Send</button>
+            <button type="submit" className="projectBtn">Send</button>
         </form>
     </div>
 
