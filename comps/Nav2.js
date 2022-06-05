9c06a3753a8d32731977aebb1dd1890e485f9d09
Ace <acekisch@gmail.com>
2022-06-04 20:34:40 -0700

    CRT theme

--- comps/Nav2.js
+++ comps/Nav2.js
@@ -1,10 +1,18 @@
 import styles from '../styles/Nav2.module.scss'
+import {useEffect, useState} from 'react'
 
 const Nav2 = () => {
 
+    //set crt theme
+    const [crt, setCrt] = useState(false)
+    useEffect(() => {
+      document.body.classList.toggle('crt', crt);
+    },[crt])
+
+
     return ( 
         <div className={styles.nav2}>
-            <i className="icon-terminal" title="CRT mode"></i>
+            <i className="icon-terminal" title="CRT mode" onClick={()=> setCrt(!crt)}></i>
         </div>
      );
 }
