1fb97f289ee6864a17fe55621542eb0a987869b6
Ace <acekisch@gmail.com>
2022-06-10 15:07:15 -0700

    add recaptcha

--- pages/contact.js
+++ pages/contact.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 import styles from '../styles/Contact.module.scss'
 
 function Contact() {
@@ -6,9 +7,12 @@ function Contact() {
   const [success, setSuccess] = useState(false);
   const [error, setError] = useState(false);
   const [hide, setHide] = useState(false);
+  const recaptchaRef = React.createRef();
 
   async function handleOnSubmit(e) {
     e.preventDefault();
+    
+    recaptchaRef.current.execute();
 
     const formData = {}
 
@@ -31,6 +35,20 @@ function Contact() {
     setHide(!hide);
   }
 
+  const onReCAPTCHAChange = (captchaCode) => {
+    // If the reCAPTCHA code is null or undefined indicating that
+    // the reCAPTCHA was expired then return early
+    if(!captchaCode) {
+      return;
+    }
+    // Else reCAPTCHA was executed successfully so proceed with the 
+    // alert
+    alert(`Hey, ${email}`);
+    // Reset the reCAPTCHA so that it can be executed again if user 
+    // submits another email.
+    recaptchaRef.current.reset();
+  }
+  
   return (
  
     <div id="contact" className="page-container" >
@@ -48,16 +66,22 @@ function Contact() {
           className={styles.contact} 
           style={{ display: hide ? "none" : "grid" }}
         >
-            <input type="hidden" name="form-name" value="contact" />
-            <label htmlFor="name" hidden>Name</label>
-            <input type="text" name="name" placeholder="Name" required/>
+          <ReCAPTCHA
+            ref={recaptchaRef}
+            size="invisible"
+            sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
+            onChange={onReCAPTCHAChange}
+          />
+          <input type="hidden" name="form-name" value="contact" />
+          <label htmlFor="name" hidden>Name</label>
+          <input type="text" name="name" placeholder="Name" required/>
 
-            <label htmlFor="email" hidden>Email</label>
-            <input type="email" name="email" placeholder="Email" required/>
+          <label htmlFor="email" hidden>Email</label>
+          <input type="email" name="email" placeholder="Email" required/>
 
-            <label htmlFor="message" hidden>Message</label>
-            <textarea name="message" placeholder="Message" required></textarea>
-            <button type="submit" className="btn">Send</button>
+          <label htmlFor="message" hidden>Message</label>
+          <textarea name="message" placeholder="Message" required></textarea>
+          <button type="submit" className="btn">Send</button>
         </form>
       </div>
     
