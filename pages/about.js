b0a28d1703ee8451099424a980c1eea1dd4a4be9
Ace <acekisch@gmail.com>
2022-10-14 21:03:52 -0700

    crt mode button hover icon color

--- pages/about.js
+++ pages/about.js
@@ -16,54 +16,54 @@ export default function About() {
         <div className="skillsBox">
           <h2>Dev</h2>
             <div>
-              <Icon className="icon-html5" title="HTML5" />
-              <Icon className="icon-css3" title="CSS3" />
-              <Icon style={{color:"#CC6699"}} className="icon-sass" title="SCSS" /> 
-              <Icon className="icon-javascript" title="Javascript" />
-              <Icon className="icon-typescript" title="Typescript" />
-              <Icon className="icon-jquery" title="jQuery" />
-              <Icon className="icon-react" title="React" />
-              {/* <Icon className="icon-redux" title="Redux" /> */}
-              <Icon className="icon-next-js" title="Next.js" />
-              <Icon className="icon-node-js" title="Node.js" />
-              <Icon className="icon-php" title="PHP" />
-              <Icon className="icon-mysql" title="MySQL" />
-              <Icon className="icon-mongodb" title="MongoDB" />
+              <Icon className="icon-html5" name="HTML5" />
+              <Icon className="icon-css3" name="CSS3" />
+              <Icon style={{color:"#CC6699"}} className="icon-sass" name="SCSS" /> 
+              <Icon className="icon-javascript" name="Javascript" />
+              <Icon className="icon-typescript" name="Typescript" />
+              <Icon className="icon-jquery" name="jQuery" />
+              <Icon className="icon-react" name="React" />
+              {/* <Icon className="icon-redux" name="Redux" /> */}
+              <Icon className="icon-next-js" name="Next.js" />
+              <Icon className="icon-node-js" name="Node.js" />
+              <Icon className="icon-php" name="PHP" />
+              <Icon className="icon-mysql" name="MySQL" />
+              <Icon className="icon-mongodb" name="MongoDB" />
             </div>
           </div>
 
           {/* <div className="skillsBox">
             <h2>Data</h2>
               <div>
-                <i className="icon-mysql" title="MySQL"></i>
-                <i className="icon-mongodb" title="MongoDB"></i>
-                <i className="icon-firebase" title="Firebase"></i>
-                <i className="icon-postgre" title="PostgreSQL"></i>
-                <i className="icon-auth0" title="Auth0"></i>
+                <i className="icon-mysql" name="MySQL"></i>
+                <i className="icon-mongodb" name="MongoDB"></i>
+                <i className="icon-firebase" name="Firebase"></i>
+                <i className="icon-postgre" name="PostgreSQL"></i>
+                <i className="icon-auth0" name="Auth0"></i>
               </div>
           </div> */}
 
           <div className="skillsBox">
             <h2>Design</h2>
               <div>
-                <Icon className="icon-photoshop" title="Photoshop" />
-                <Icon className="icon-lightroom" title="Lightroom" />
-                <Icon className="icon-illustrator" title="Illustrator" />
-                <Icon className="icon-xd" title="Experience Design" />
-                <Icon className="icon-sketch" title="Sketch" style={{color:"#F7B500"}} />
-                <Icon className="icon-figma" title="Figma" />
+                <Icon className="icon-photoshop" name="Photoshop" />
+                <Icon className="icon-lightroom" name="Lightroom" />
+                <Icon className="icon-illustrator" name="Illustrator" />
+                <Icon className="icon-xd" name="Experience Design" />
+                <Icon className="icon-sketch" name="Sketch" style={{color:"#F7B500"}} />
+                <Icon className="icon-figma" name="Figma" />
               </div>
           </div>
 
           <div className="skillsBox">
             <h2>Deploy</h2>
               <div>
-              <Icon className="icon-netlify" title="Netlify" />
-              <Icon className="icon-vercel" title="Vercel" style={{color:'#fff'}} />
-              <Icon className="icon-digitalocean" title="DigitalOcean" />
-              <Icon className="icon-aws" title="AWS" />
-              <Icon className="icon-heroku" title="Heroku" />
-              <Icon className="icon-wpengine" title="WPEngine" />
+              <Icon className="icon-netlify" name="Netlify" />
+              <Icon className="icon-vercel" name="Vercel" style={{color:'#fff'}} />
+              <Icon className="icon-digitalocean" name="DigitalOcean" />
+              <Icon className="icon-aws" name="AWS" />
+              <Icon className="icon-heroku" name="Heroku" />
+              <Icon className="icon-wpengine" name="WPEngine" />
                 
               </div>
           </div>
@@ -80,13 +80,13 @@ function Icon(props){
     <div className="icon-wrapper">
       <i 
         className={`${props.className} icon ${iconText ? `icon-active` : null}`}
-        title={props.title} 
+        name={props.name} 
         // onClick={() => setIconText(!iconText)}
         onMouseEnter={() => setIconText(true)}
         onMouseLeave={() => setIconText(false)}
       >
       </i>
-      <span className={`icon_text ${iconText ? `icon_text--active` : null}`}>{props.title}</span>
+      <span className={`icon_text ${iconText ? `icon_text--active` : null}`}>{props.name}</span>
     </div>
   )
 }
