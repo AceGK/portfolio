1842112bfd41d2c9ea5af9250fb01d4fc1a4e1b3
Ace <acekisch@gmail.com>
2023-03-12 14:13:22 -0700

    update projects, fix active nav item hidden border

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -8,7 +8,6 @@ import Hal from '../../public/svg/hal9000m.svg';
 import Tipper from '../../public/svg/tipper.svg';
 import IP from '../../public/svg/ip-tracker.svg';
 
-
 export default class ProjectData extends Component {
   state = {
     projects: [
@@ -16,17 +15,14 @@ export default class ProjectData extends Component {
         title: 'This Site',
         logo: <AK style={{ fill: 'white' }} />,
         icons: [
-          <i className="icon-html5" title="HTML5" key="1"></i>,
-          <i className="icon-sass" title="SCSS" style={{ color: "#CC6699" }} key="2"></i>,
-          <i className="icon-javascript" title="Javascript" key="3"></i>,
-          <i className="icon-react" title="React" key="4"></i>,
-          <i className="icon-next-js" title="Next.js" key="5"></i>,
-          <i className="icon-node-js" title="Node.js" key="6"></i>
+          <i className="icon-next-js" title="Next.js" key="1" />,
+          <i className="icon-sass" title="SCSS" key="2"/>,
+          <i className="icon-sendgrid" title="Sendgrid" key="3"/>,
         ],
         description: 'My portfolio site, showcasing my work and personal projects',
         buttons: [
           <Link href="https://github.com/AceGK/portfolio" className="btn" target="_blank" rel="noreferrer" key="1">
-            <i className="icon-github" title="Github"></i> Github
+            <i className="icon-github" title="Github"/> Github
           </Link>
         ],
         id: 1
@@ -35,16 +31,15 @@ export default class ProjectData extends Component {
         title: 'Purple Lotus',
         logo: <PLPC />,
         icons: [
-          <i className="icon-html5" title="HTML5" key="1"></i>,
-          <i className="icon-css3" title="CSS3" key="2"></i>,
-          <i className="icon-javascript" title="Javascript" key="3"></i>,
-          <i className="icon-wordpress" title="Wordpress" key="4"></i>,
-          <i className="icon-php" title="php" key="5"></i>,
+          <i className="icon-next-js" title="Next.js" key="1" />,
+          <i className="icon-sass" title="SCSS" key="2"/>,
+          <i className="icon-algolia" title="Algolia" key="3" />,
+          <i className="icon-sanity" title="Sanity" key="4" />,
         ],
-        description: 'Ecommerce website for a cannabis dispensary located in San Jose, CA. Currently being rebuilt with Next.js',
+        description: 'Work in progress. Ecommerce site for a cannabis dispensary built with Next.js, Algolia Instant Search, and Sanity CMS',
         buttons: [
           <Link href="https://plpcsanjose.com" className="btn" target="_blank" rel="noreferrer" key="1">
-            <i className="icon-link" title="view site"></i> View
+            <i className="icon-link" title="view site" /> View
           </Link>,
         ],
         id: 2
@@ -53,20 +48,16 @@ export default class ProjectData extends Component {
         title: 'IP Tracker',
         logo: <IP style={{ fill: 'white' }} />,
         icons: [
-          <i className="icon-html5" title="HTML5" key="1"></i>,
-          <i className="icon-sass" title="SCSS" style={{ color: "#CC6699" }} key="2"></i>,
-          <i className="icon-javascript" title="Javascript" key="3"></i>,
-          <i className="icon-react" title="React" key="4"></i>,
-          <i className="icon-next-js" title="Next.js" key="5"></i>,
-          <i className="icon-node-js" title="Node.js" key="6"></i>
+          <i className="icon-next-js" title="Next.js" key="1" />,
+          <i className="icon-sass" title="SCSS" key="2"/>,
         ],
         description: 'IP address tracker built with NextJS, Ipify Geolocation API, and LeafletJS',
         buttons: [
           <Link href="https://ip-tracker-acegk.vercel.app/" className="btn" target="_blank" rel="noreferrer" key="1">
-            <i className="icon-link" title="view site"></i> View
+            <i className="icon-link" title="view site"/> View
           </Link>,
           <Link href="https://github.com/AceGK/ip-tracker" className="btn" target="_blank" rel="noreferrer" key="2">
-            <i className="icon-github" title="Github"></i> Github
+            <i className="icon-github" title="Github"/> Github
           </Link>
         ],
         id: 3
@@ -75,17 +66,17 @@ export default class ProjectData extends Component {
         title: 'Tip Calculator',
         logo: <Tipper style={{ fill: 'white' }} />,
         icons: [
-          <i className="icon-html5" title="HTML5" key="1"></i>,
-          <i className="icon-css3" title="CSS3" key="2"></i>,
-          <i className="icon-javascript" title="Javascript" key="3"></i>,
+          <i className="icon-html5" title="HTML5" key="1" />,
+          <i className="icon-css3" title="CSS3" key="2" />,
+          <i className="icon-javascript" title="Javascript" key="3" />,
         ],
         description: 'A vanilla JS tip calculator that evaluates cost per person based on bill amount and tip %',
         buttons: [
           <Link href="https://acegk.github.io/tip-calculator/" className="btn" target="_blank" rel="noreferrer" key="1">
-            <i className="icon-link" title="view site"></i> View
+            <i className="icon-link" title="view site"/> View
           </Link>,
           <Link href="https://github.com/AceGK/tip-calculator" className="btn" target="_blank" rel="noreferrer" key="2">
-            <i className="icon-github" title="Github"></i> Github
+            <i className="icon-github" title="Github"/> Github
           </Link>
         ],
         id: 4
@@ -94,13 +85,13 @@ export default class ProjectData extends Component {
         title: 'HAL PC monitor',
         logo: <Hal />,
         icons: [
-          <i className="icon-rainmeter" title="Rainmeter" key="1"></i>,
-          <i className="icon-lua" title="LUA" key="2"></i>
+          <i className="icon-rainmeter" title="Rainmeter" key="1"/>,
+          <i className="icon-lua" title="LUA" key="2"/>
         ],
-        description: 'Rainmeter skin modeled after HAL 9000 that monitors PC performance status and details',
+        description: 'Hal 9000 themed PC hardware usage meters',
         buttons: [
           <Link href="https://github.com/AceGK/Hal9000-Rainmeter" className="btn" target="_blank" rel="noreferrer" key="1">
-            <i className="icon-github" title="Github"></i> Github
+            <i className="icon-github" title="Github"/> Github
           </Link>,
         ],
         id: 5
